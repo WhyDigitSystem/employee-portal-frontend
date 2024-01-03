f86781f507ab6d63445e6cb1ba8411fd3dc68181
karupuwds <karupu@whydigit.com>
2024-01-04 04:10:20 +0530

    Permission Request screen design change done

--- src/features/flowScreens/permissionRequest/NewPermissionRequest.js
+++ src/features/flowScreens/permissionRequest/NewPermissionRequest.js
@@ -34,7 +34,7 @@ function NewPermissionRequest({ newPermissionRequest }) {
   return (
     <div className="card w-full p-6 bg-base-100 shadow-xl">
       <div className="d-flex justify-content-between">
-        <h1 className="text-xl font-semibold mb-3">New Permission</h1>
+        <h1 className="text-xl font-semibold mb-3">New Permission Request</h1>
         <IoMdClose
           onClick={handleClosePermission}
           type="button"
@@ -139,7 +139,7 @@ function NewPermissionRequest({ newPermissionRequest }) {
         </button>
         <button
           type="button"
-          //onClick={handleCustomerClose}
+          onClick={handleClosePermission}
           className="bg-blue inline-block rounded bg-primary h-fit px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
         >
           Cancel
--- src/pages/protected/PermissionRequest.js
+++ src/pages/protected/PermissionRequest.js
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setPageTitle } from "../../features/common/headerSlice";
-import { PermissionRequest } from "../../features/flowScreens/PermissionRequest";
+import { PermissionRequest } from "../../features/flowScreens/permissionRequest/PermissionRequest";
+//import { PermissionRequest } from "../../features/flowScreens/PermissionRequest";
 
 function InternalPage() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(setPageTitle({ title: "Permission Request" }));
+    dispatch(setPageTitle({ title: "Permission Report" }));
   }, []);
 
   return <PermissionRequest />;
