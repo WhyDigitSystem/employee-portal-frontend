7b54896a480f9a7147c113c189455302cee0f842
Vignesh <vignesh@whydigit.com>
2024-01-04 10:03:47 +0530

    Holiday complete

--- src/features/masters/Holidays.js
+++ src/features/masters/Holidays.js
@@ -1,5 +1,114 @@
-import React from "react";
-
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import React, { useState } from "react";
+import { AiOutlineSearch, AiOutlineWallet } from "react-icons/ai";
+import { BsListTask } from "react-icons/bs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
 export const Holidays = () => {
-  return <div>Holidays</div>;
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabSelect = (index) => {
+    setTabIndex(index);
+  };
+  const buttonStyle = {
+    fontSize: "20px", // Adjust the font size as needed save
+  };
+
+  return (
+    <>
+      <div>
+        <div className="card w-full p-6 bg-base-100 shadow-xl">
+          <div className="row d-flex justify-content-center align-items-center">
+            <div className="d-flex flex-wrap justify-content-start">
+              <button className="btn btn-ghost btn-sm normal-case col-xs-2">
+                <AiOutlineWallet style={buttonStyle} />
+                <span className="ml-1">New</span>
+              </button>
+              <button className="btn btn-ghost btn-sm normal-case col-xs-2">
+                <AiOutlineSearch style={buttonStyle} />
+                <span className="ml-1">Search</span>
+              </button>
+              <button className="btn btn-ghost btn-sm normal-case col-xs-2">
+                <BsListTask style={buttonStyle} />
+                <span className="ml-1">List View</span>
+              </button>
+            </div>
+            <div className="row d-flex mt-3">
+              <div className="col-md-4">
+                <FormControl fullWidth>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      label="Date"
+                      slotProps={{
+                        textField: { size: "small", clearable: true },
+                      }}
+                      //value={boDate}
+                      //onChange={(newValue) => setBoDate(newValue)}
+                    />
+                  </LocalizationProvider>
+                </FormControl>
+              </div>
+              <div className="col-md-4">
+                <FormControl fullWidth variant="filled">
+                  <TextField
+                    id="festival"
+                    label="Festival"
+                    size="small"
+                    //placeholder="accountcode"
+                    inputProps={{ maxLength: 30 }}
+                  />
+                </FormControl>
+              </div>
+              <div className="col-md-4">
+                <FormControl fullWidth variant="filled">
+                  <TextField
+                    id="remarks"
+                    label="Remarks"
+                    size="small"
+                    //placeholder="accountcode"
+                    inputProps={{ maxLength: 30 }}
+                  />
+                </FormControl>
+              </div>
+              <div className="col-md-4 mb-3">
+                <FormGroup>
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="Active"
+                  />
+                </FormGroup>
+              </div>
+            </div>
+
+            <div className="d-flex flex-row mt-3">
+              <button
+                type="button"
+                //onClick={handleCustomer}
+                className="bg-blue me-5 inline-block rounded bg-primary h-fit px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              >
+                Save
+              </button>
+              <button
+                type="button"
+                //onClick={handleCustomerClose}
+                className="bg-blue inline-block rounded bg-primary h-fit px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
