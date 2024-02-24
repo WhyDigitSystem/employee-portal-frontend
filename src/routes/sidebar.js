391b88b80da9747148f1c0ec53b9c554af150ad0
karupuwds <karupu@whydigit.com>
2024-02-24 10:30:39 +0530

    sidebar changeed done

--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -72,16 +72,16 @@ const routes = [
         icon: <CalendarDaysIcon className={iconClasses} />, // icon component
         name: "Leave Process", // name that appear in Sidebar
       },
-      {
-        path: "/app/jpcheckincheckout", // url
-        icon: <CalendarDaysIcon className={iconClasses} />, // icon component
-        name: "JP CheckIn CheckOut", // name that appear in Sidebar
-      },
-      {
-        path: "/app/organizationsetup", // url
-        icon: <CalendarDaysIcon className={iconClasses} />, // icon component
-        name: "Orginazation Setup", // name that appear in Sidebar
-      },
+      // {
+      //   path: "/app/jpcheckincheckout", // url
+      //   icon: <CalendarDaysIcon className={iconClasses} />, // icon component
+      //   name: "JP CheckIn CheckOut", // name that appear in Sidebar
+      // },
+      // {
+      //   path: "/app/organizationsetup", // url
+      //   icon: <CalendarDaysIcon className={iconClasses} />, // icon component
+      //   name: "Orginazation Setup", // name that appear in Sidebar
+      // },
     ],
   },
   // transaction Folder
